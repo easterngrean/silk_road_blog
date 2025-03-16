159a5cd0fed804e40c3ffe9602fba8542b269f29
easterngrean <42080907+easterngrean@users.noreply.github.com>
2025-03-16 14:13:18 +0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
+// Grab elements
+const selectElement = selector => {
+  const element = document.querySelector(selector)
+  if(element) return element;
+  throw new Error(`Something went wrong, make sure that the ${selector} exists or is typed correctly.`
+                  };
 
+console.log(selectElement('.navbar'));
+
+                  
+//Nav styles on scroll
+
+// Open menu & search pop-up
+
+// Open/Close search form popup
+
+// -- Close the search form popup on ESC keypress
+
+// Switch theme/add to local storage
+
+// Swiper
