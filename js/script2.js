f6ec6c169ecda45f1a5b1521bc740c8ab9914714
easterngrean <42080907+easterngrean@users.noreply.github.com>
2025-03-16 18:10:17 +0400

    Update script2.js

--- js/script2.js
+++ js/script2.js
@@ -5,8 +5,9 @@ const selectElement = (selector) => {
   throw new Error(`Something went wrong! make sure that the ${selector} exists or is typed correctly.`);
       }
 
+console.log(selectElement('.navbar'));
 
-//Nav styles on scroll
+/* Nav styles on scroll
 const scrollHeader = () => { 
   const headerElement = selectElement('#header');
   if(this.scrollY >= 15) {
@@ -16,6 +17,6 @@ const scrollHeader = () => {
     headerElement.classList.remove('activated');
 }
 
-window.addEventListener('scroll', scrollHeader);
+window.addEventListener('scroll', scrollHeader); */
 
-console.log(selectElement('.navbar'));
+
