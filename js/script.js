485deb88cec0a218a5314143ced44a74db9f228f
easterngrean <42080907+easterngrean@users.noreply.github.com>
2025-03-16 14:18:29 +0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ const selectElement = selector => {
   const element = document.querySelector(selector)
   if(element) return element;
   throw new Error(`Something went wrong, make sure that the ${selector} exists or is typed correctly.`
-                  };
+      };
 
 console.log(selectElement('.navbar'));
 
