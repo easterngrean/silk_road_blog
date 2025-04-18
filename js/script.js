eb4f680e14e36ccf263e3887ce6e4f0abf5e8aa4
easterngrean <42080907+easterngrean@users.noreply.github.com>
2025-04-18 12:34:40 +0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -24,6 +24,7 @@
 
   // Example: load multiple posts by calling the function for each post
   loadPost('posts/130425.html');
+  loadPost('posts/180425.html');
   // ... add more calls as needed.
 
   // Function that loads a links from the homepage into each post
